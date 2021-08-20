a2d9476d0d69a35fe9ac3367fee890e0ee2dbda1
Yash Panchal <87405211+YashBatBoi@users.noreply.github.com>
2021-08-20 10:15:56 +0530

    Create  Path Sum.js

---
+++ Path Sum.js
@@ -0,0 +1,16 @@
+// Given the root of a binary tree and an integer targetSum, 
+// return true if the tree has a root-to-leaf path such that 
+// adding up all the values along the path equals targetSum.
+
+// A leaf is a node with no children.
+
+
+var hasPathSum = function(root, targetSum) {
+    if (root === null) return false;
+    
+    if (!root.left && !root.right) {
+        return root.val === targetSum;
+    }
+    targetSum -= root.val;
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+};
